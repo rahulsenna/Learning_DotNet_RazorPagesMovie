5754492d1b0affa17a8f11cf0e7b4c2899efd19b
Rahul Senna <rahulvsenna@gmail.com>
2025-09-16 18:03:51 +0530

    Work with a Database (SeedData)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,6 +19,12 @@ else
 }
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    IServiceProvider services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
