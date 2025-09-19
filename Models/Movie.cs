98f4af02481f21b7326a8676015872f54fcd4e4f
Rahul Senna <rahulvsenna@gmail.com>
2025-09-19 17:33:40 +0530

    Add validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,15 +6,19 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
+    [StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; }
 
-    [DataType(DataType.Date)]
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
 
-    [Column(TypeName = "decimal(18, 2)")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+    public string Genre { get; set; }
+
+    [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
     public string Rating { get; set; } = string.Empty;
 }
